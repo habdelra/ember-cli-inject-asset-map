e7ab2972607845f365583cef7c9b968f394adfb9
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2016-03-17 10:27:09 -0400

    more dynamic tree handling

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ module.exports = {
     var fs          = require('fs'),
         path        = require('path'),
         tree        = results['graph']['tree'],
-        assetMap    = tree._inputNodes[ process.env.EMBER_CLI_FASTBOOT ? 1 : 0].assetMap,
+        assetMap    = tree._inputNodes[tree._inputNodes.length - 1].assetMap,
         jsPath      = path.join(results.directory, assetMap['assets/' + process.env.ASSET_MAP_APP_NAME + '.js']),
         js          = fs.readFileSync(jsPath, 'utf-8'),
         assetMapKey = 'assetMapHash',
         expression  = new RegExp(assetMapKey + ':\\s?(void 0|undefined)'),
         injectedJs  = js.replace(expression, assetMapKey + ': ' + JSON.stringify(assetMap));
 
-    var success = true;
     if (fs.existsSync(jsPath)) {
       fs.writeFileSync(jsPath, injectedJs, 'utf-8');
     } else {
-      success = false;
       console.error('Unable to inject asset map. File "' + jsPath + '" does not exist.');
     }
   }
